81ce99ecfbf56d26a960fbece91c886126f8a125
juan <aragonlopezjuan87@gmail.com>
2021-01-06 22:20:29 +0100

    deploy

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const app: Express = express()
 
-const PORT: string | number = process.env.PORT || 5000
+const PORT: string | number = process.env.PORT || 3000
 
 app.use(cors())
 app.use(bodyParser.json()); 
